c6c336c87fdfbe3c0d447d251771f8478199654e
ValentinV <valentin.vranjes@gmail.com>
2023-04-13 19:22:44 +0200

    zadnji commit danas

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import logo from '../assets/images/logo.svg';
 import '../assets/styles/App.css';
 
+import randomstring from 'randomstring';
+
 function App() {
+var rndStr = randomstring.generate({
+  length: 8,
+});
+console.log("Random string = " + rndStr);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Moja prva aplikacija
+          Moja prva aplikacija, random string={rndStr}
         </p>
         <a
           className="App-link"
